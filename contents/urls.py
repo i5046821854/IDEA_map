4cd4d11a594e2c61417ee7c1bf5afe28538826f4
lee <o5046821854@gmail.com>
2021-06-30 23:33:25 +0900

    first commit

---
+++ contents/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('<str:content_id>',detailPlace, name = "detailPlace"),
+    path('addPlace/', addPlace, name = "addPlace"),
+    path('create/', create, name = "create"),
+    path('editPlace/<str:content_id>', editPlace, name="editPlace"),
+    path('updatePlace/<str:content_id>', updatePlace, name = "updatePlace"),
+    path('delete/<str:content_id>', deletePlace, name = "deletePlace"),
+    path('myMap/<str:Category>', myMap, name = "myMap"),
+    path('newCategory/', newCategory, name = "newCategory"),
+]
